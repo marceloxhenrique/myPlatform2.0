3a46a50948bb079d399a74ea4cd1576afd351c84
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-10-22 21:32:53 +0200

    feat: Add a teacher mode and create a switch button

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,5 @@
+const CoursePage = () => {
+  return <div>Course CoursePage</div>;
+};
+
+export default CoursePage;
--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
 import { SidebarItem } from "./sidebar-item";
+import { usePathname } from "next/navigation";
 const guestRoutes = [
   {
     icon: Layout,
@@ -13,9 +14,24 @@ const guestRoutes = [
     href: "/search",
   },
 ];
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
 
 export const SidebarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
   return (
     <div className="flex flex-col w-full">
       {routes.map((route) => (
