c641a24c3f3b01572a3d6115d07d26e08cd3f913
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-11-15 00:59:07 +0100

    feat: Update sidebar-routes, add a create button for AdminUser

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { BookOpenText, Layout } from "lucide-react";
+
+import { BookOpenText, Layout, PencilRuler } from "lucide-react";
 import { SidebarItem } from "./sidebar-item";
+import { useAuth } from "@clerk/nextjs";
 
 const guestRoutes = [
   {
@@ -15,17 +17,47 @@ const guestRoutes = [
   },
 ];
 
+const teacherRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: BookOpenText,
+    label: "Courses",
+    href: "/course/courses",
+  },
+  {
+    icon: PencilRuler,
+    label: "Create",
+    href: "/course/create",
+  },
+];
+const isTeacher = process.env.NEXT_PUBLIC_TEACHER_ID;
+
 export const SidebarRoutes = () => {
+  const { userId } = useAuth();
+
   return (
     <div className="flex flex-col w-full">
-      {guestRoutes.map((route) => (
-        <SidebarItem
-          key={route.href}
-          icon={route.icon}
-          label={route.label}
-          href={route.href}
-        />
-      ))}
+      {userId === isTeacher
+        ? teacherRoutes.map((route) => (
+            <SidebarItem
+              key={route.href}
+              icon={route.icon}
+              label={route.label}
+              href={route.href}
+            />
+          ))
+        : guestRoutes.map((route) => (
+            <SidebarItem
+              key={route.href}
+              icon={route.icon}
+              label={route.label}
+              href={route.href}
+            />
+          ))}
     </div>
   );
 };
