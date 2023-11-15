de60ee89396e22d3b6ff991fd322973f9e82cc85
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-11-15 01:03:00 +0100

    feat: Add aria-controls in Sheet at mobile-sidebar component, and delete analytics page

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,10 +1,14 @@
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "./sidebar";
+
 export const MobileSidebar = () => {
   return (
     <Sheet>
-      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+      <SheetTrigger
+        className="md:hidden pr-4 hover:opacity-75 transition"
+        aria-controls="radix-:R2mpj9:"
+      >
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-white">
