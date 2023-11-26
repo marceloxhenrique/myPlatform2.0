413c856afebd84532bc86bbb0785bea6ba06412e
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-11-26 18:24:53 +0100

    feat: add table components from shadcnui

--- lib/teacher.ts
+++
@@ -1,6 +0,0 @@
-export const isTeacher = (userId?: string | undefined | null) => {
-  const isTeacher =
-    userId === process.env.NEXT_PUBLIC_TEACHER_ID1 ||
-    userId === process.env.NEXT_PUBLIC_TEACHER_ID2;
-  return isTeacher;
-};
