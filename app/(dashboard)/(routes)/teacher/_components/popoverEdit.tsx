929d2b2e0e319e956807b87aa858b1d0fe94d99a
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-11-28 23:23:42 +0100

    fix: resolve bug issue in popoverEdit component. Add a aria-controls to PopoverTriger  to fix the issue radix-:R47jcrbdmcq:

--- app/(dashboard)/(routes)/teacher/_components/popoverEdit.tsx
+++ app/(dashboard)/(routes)/teacher/_components/popoverEdit.tsx
@@ -8,7 +8,9 @@ import { Pencil, Trash2 } from "lucide-react";
 const PopOver = () => {
   return (
     <Popover>
-      <PopoverTrigger className="text-2xl">...</PopoverTrigger>
+      <PopoverTrigger className="text-2xl" aria-controls="radix-:R47jcrbdmcq:">
+        ...
+      </PopoverTrigger>
       <PopoverContent className="flex flex-col justify-items-start gap-2">
         <EditCourseButton title="Edit" icon={<Pencil />} />
         <EditCourseButton title="Delete" icon={<Trash2 />} />
