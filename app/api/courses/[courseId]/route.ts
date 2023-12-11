2a4c2b75521edfa06d6ce0d54ab975a8e2aaba8e
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-12-11 23:56:07 +0100

    feat: `implement POST, GET and DELETE routes` for course management. - Added a DELETE route to handle the deletion of a course. - Added a POST route for creating a new course. - Added a GET route for retrieving all courses.

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db as Prisma } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { isTeacher } from "@/lib/isTeacher";
 
 export async function DELETE(
   request: NextRequest,
@@ -8,15 +10,22 @@ export async function DELETE(
   const courseId = params.courseId;
   if (courseId) {
     try {
+      const { userId } = auth();
+
+      if (!userId || !isTeacher(userId)) {
+        return NextResponse.json("Not Authorized", { status: 401 });
+      }
       const res = await Prisma.course.delete({
         where: {
           id: courseId,
         },
       });
-      return NextResponse.json(`Course ${res.title} successfuly deleted`);
+      return NextResponse.json(`Course ${res.title} successfuly deleted`, {
+        status: 201,
+      });
     } catch (error) {
       return NextResponse.json(`Unable to delete the course Error: ${error}`);
     }
   }
-  return NextResponse.json(`Course doens't exist`);
+  return NextResponse.json(`Unable to retrive data`, { status: 500 });
 }
--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,28 +1,57 @@
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
 import { db as Prisma } from "@/lib/db";
+import { getAllCourse } from "@/actions/getAllCourses";
+import { isTeacher } from "@/lib/isTeacher";
 
 export async function POST(request: NextRequest) {
-  const { title } = await request.json();
-  if (!title) {
-    return NextResponse.json({ error: "Invalid data" }, { status: 404 });
-  }
+  try {
+    const { userId } = auth();
+    const { title } = await request.json();
 
-  const isCourseExist = await Prisma.course.findUnique({
-    where: {
-      title: title,
-    },
-  });
-  if (isCourseExist) {
+    if (!userId || !isTeacher(userId)) {
+      return NextResponse.json("Not Authorized", { status: 401 });
+    }
+
+    if (!title) {
+      return NextResponse.json({ error: "Invalid data" }, { status: 404 });
+    }
+
+    const isCourseExist = await Prisma.course.findUnique({
+      where: {
+        title: title,
+      },
+    });
+    if (isCourseExist) {
+      return NextResponse.json(
+        { error: "A Course with the same name already exist" },
+        { status: 400 },
+      );
+    }
+    await Prisma.course.create({
+      data: {
+        title: title,
+      },
+    });
+    return NextResponse.json("Course succesfully created!", { status: 201 });
+  } catch (error) {
+    return NextResponse.json("Unable to create course", { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const { userId } = auth();
+    const getCourses = await getAllCourse();
+
+    if (!userId) {
+      return NextResponse.json("Not Authorized", { status: 401 });
+    }
+    return NextResponse.json({ courses: getCourses }, { status: 201 });
+  } catch (error) {
     return NextResponse.json(
-      { error: "A Course with the same name already exist" },
-      { status: 400 },
+      "Unable to retrive data. Please try again later" + error,
+      { status: 500 },
     );
   }
-  await Prisma.course.create({
-    data: {
-      title: title,
-    },
-  });
-
-  return NextResponse.json("Course succesfully created!");
 }
